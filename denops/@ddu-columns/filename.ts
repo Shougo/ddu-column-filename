6e5e1c77ca0003e0244044e00b773e78495f2a22
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-02-27 16:25:41 +0900

    Print symbolic link path

--- denops/@ddu-columns/filename.ts
+++ denops/@ddu-columns/filename.ts
@@ -2,10 +2,10 @@ import {
   BaseColumn,
   DduItem,
   ItemHighlight,
-} from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
-import { GetTextResult } from "https://deno.land/x/ddu_vim@v2.0.0/base/column.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.0.0/deps.ts";
-import { basename } from "https://deno.land/std@0.165.0/path/mod.ts";
+} from "https://deno.land/x/ddu_vim@v2.3.0/types.ts";
+import { GetTextResult } from "https://deno.land/x/ddu_vim@v2.3.0/base/column.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.3.0/deps.ts";
+import { basename } from "https://deno.land/std@0.178.0/path/mod.ts";
 
 type Params = {
   collapsedIcon: string;
@@ -35,12 +35,24 @@ export class Column extends BaseColumn<Params> {
     items: DduItem[];
   }): Promise<number> {
     const widths = await Promise.all(args.items.map(
-      async (item) =>
-        item.__level + 1 +
-        (await fn.strwidth(
+      async (item) => {
+        const action = item?.action as ActionData;
+        const isLink = action.isLink ?? false;
+        const isDirectory = item.isTree ?? false;
+        let path = basename(action.path ?? item.word) +
+          (isDirectory ? "/" : "");
+
+        if (isLink && action.path) {
+          path += ` -> ${await Deno.realPath(action.path)}`
+        }
+
+        const length = item.__level + 1 + (await fn.strwidth(
           args.denops,
-          args.columnParams.iconWidth + (item.display ?? item.word),
-        ) as number),
+          args.columnParams.iconWidth + path,
+        ) as number);
+
+        return length;
+      },
     )) as number[];
     return Math.max(...widths);
   }
@@ -56,8 +68,12 @@ export class Column extends BaseColumn<Params> {
     const highlights: ItemHighlight[] = [];
     const isDirectory = args.item.isTree ?? false;
     const isLink = action.isLink ?? false;
-    const path = basename(action.path ?? args.item.word) +
+    let path = basename(action.path ?? args.item.word) +
       (isDirectory ? "/" : "");
+
+    if (isLink && action.path) {
+      path += ` -> ${await Deno.realPath(action.path)}`
+    }
 
     if (isDirectory) {
       const userHighlights = args.columnParams.highlights;
@@ -101,6 +117,7 @@ export class Column extends BaseColumn<Params> {
       : isLink
       ? args.columnParams.linkIcon
       : " ";
+
     const text = " ".repeat(args.item.__level) + icon + " " + path;
     const width = await fn.strwidth(args.denops, text) as number;
     const padding = " ".repeat(args.endCol - args.startCol - width);
